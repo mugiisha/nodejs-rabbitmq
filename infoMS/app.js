aca793db8c2d70f8bfbd0e942c5ce854146c48d0
mugiisha <mugishabankz3@gmail.com>
2023-03-26 15:48:20 +0200

    infoMS creation

---
+++ infoMS/app.js
@@ -0,0 +1,21 @@
+import amqp from "amqplib";
+import config from "./config.js";
+
+async function consumeMessage() {
+  const connection = await amqp.connect("amqp://localhost");
+  const channel = await connection.createChannel();
+
+  const exchangeName = config.rabbitMQ.exchangeName;
+  await channel.assertExchange(exchangeName, "direct");
+
+  const q = await channel.assertQueue("infoQueue");
+  await channel.bindQueue(q.queue, exchangeName, "Info");
+
+  channel.consume(q.que, (msg) => {
+    const data = JSON.parse(msg.content);
+    console.log(data);
+    channel.ack(msg);
+  });
+}
+
+consumeMessage();
